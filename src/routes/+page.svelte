d445162c2eda7f47b0a2e28c6f053a8767ef8eb3
Calliope2 <konradm.studium@gmail.com>
2024-09-28 10:58:22 +0200

    Add simple Start-Screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-  
+    // You can add any necessary TypeScript code here
 </script>
 
-<main>
-  <h1 class="font-bold text-2xl text-yellow-500">OeffiFair</h1>
+<main class="flex flex-col justify-center items-center h-screen bg-neutral-200">
+    <h1 class="font-bold text-4xl text-neutral-700 mb-4">ÖffiFair</h1>
+    <button class="px-6 py-3 text-white bg-blue-500 rounded-lg hover:bg-blue-700 transition-colors" on:click={() => alert('Start button clicked!')}>
+        Start
+    </button>
 </main>
