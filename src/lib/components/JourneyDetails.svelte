cde0a61d310417aba91c64f9ae0a11e1290be808
Calliope2 <konradm.studium@gmail.com>
2024-11-11 19:03:10 +0100

    Design fixes

--- src/lib/components/JourneyDetails.svelte
+++ src/lib/components/JourneyDetails.svelte
@@ -14,48 +14,48 @@
 {#if !showEndScreen}
 <main class="flex flex-col justify-center items-center h-screen bg-neutral-200">
   <div class="space-y-16 pb-20">
-    <div class="text-3xl font-semibold">
+    <div class="text-4xl text-neutral-700 font-semibold">
       Hallo Max 👋
     </div>
     
     <div class="space-y-6">
       <div class="flex items-center space-x-3">
-        <MapPin class="w-7 h-7 text-gray-500" />
-        <span class="font-medium text-xl">Frohnau</span>
+        <MapPin class="w-8 h-8 text-neutral-700" />
+        <span class="font-medium text-2xl">Frohnau</span>
       </div>
       
       <div class="flex items-center space-x-3">
-        <TrainFront class="w-6 h-6 text-gray-500" />
-        <span>12 Haltestellen</span>
+        <TrainFront class="w-7 h-7 text-neutral-700" />
+        <span class="font-medium text-xl">12 Haltestellen</span>
       </div>
       
       <div class="flex items-center space-x-3">
-        <TrainTrack class="w-6 h-6 text-gray-500" />
-        <span>16,7 km</span>
+        <TrainTrack class="w-7 h-7 text-neutral-700" />
+        <span class="font-medium text-xl">16,7 km</span>
       </div>
       
       <div class="flex items-center space-x-3">
-        <MapPin class="w-7 h-7 text-gray-500" />
-        <span class="font-medium text-xl">Friedrichstraße</span>
+        <MapPin class="w-8 h-8 text-neutral-700" />
+        <span class="font-medium text-2xl">Friedrichstraße</span>
       </div>
       
       <div class="flex items-center space-x-3 pt-5">
-        <Euro class="w-6 h-6 text-gray-500" />
-        <span class="text-xl font-medium">2,70 €</span>
+        <Euro class="w-7 h-7 text-neutral-700" />
+        <span class="text-xl font-medium">2,70</span>
       </div>
     </div>
   </div>
   
   <button
     on:click={handleClick}
-    class="fixed bottom-0 left-0 w-full px-6 py-3 text-white bg-blue-500 rounded-lg hover:bg-blue-700 transition-colors"
+    class="fixed bottom-0 left-0 w-full px-6 py-3 text-neutral-100 text-xl bg-blue-500 rounded-lg hover:bg-blue-700 transition-colors"
   >
     Bis zum nächsten Ride!
   </button>
 </main>
 {:else}
   <div class="min-h-screen bg-neutral-200 p-6 flex flex-col justify-center items-center" in:fade={{ duration: 300 }}>
-    <h1 class="text-3xl font-semibold mb-4">ÖffiFair</h1>
-    <p class="text-xl text-gray-600">Thank you for your ride!</p>
+    <h1 class="text-5xl font-semibold mb-8">ÖffiFair</h1>
+    <p class="text-xl text-neutral-700">Thank you for your ride!</p>
   </div>
 {/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <main class="flex flex-col justify-center items-center h-screen bg-neutral-200">
-    <h1 class="font-bold text-4xl text-neutral-700 mb-4">ÖffiFair</h1>
-    <a href="/mainscreen" class="px-6 py-3 text-white bg-blue-500 rounded-lg hover:bg-blue-700 transition-colors">
+    <h1 class="font-bold text-5xl text-neutral-700 mb-8">ÖffiFair</h1>
+    <a href="/mainscreen" class="px-8 py-4 text-neutral-100 text-xl bg-blue-500 rounded-lg hover:bg-blue-700 transition-colors">
         Start
     </a>
 </main>
